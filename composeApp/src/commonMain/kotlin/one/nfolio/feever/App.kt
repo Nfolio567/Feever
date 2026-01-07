4869b02e507e39eb6326ec52e15a0ebb2f6b2ec9
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2026-01-07 13:28:19 +0900

    feed menu with null icon

--- composeApp/src/commonMain/kotlin/one/nfolio/feever/App.kt
+++ composeApp/src/commonMain/kotlin/one/nfolio/feever/App.kt
@@ -1,10 +1,9 @@
 package one.nfolio.feever
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,26 +11,32 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import feever.composeapp.generated.resources.Manrope
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import feever.composeapp.generated.resources.Res
-import feever.composeapp.generated.resources.compose_multiplatform
+import feever.composeapp.generated.resources.nullIcon
+import org.jetbrains.compose.resources.Font
 
 @Composable
 @Preview
@@ -70,14 +75,16 @@ fun App() {
         .fillMaxSize()
         .background(Color.LightGray)
     ) {
+      val headerRadius = 10.dp
+      val manrope = Res.font.Manrope
 
       Column(
         modifier = Modifier
           .background(
             color = Color.White,
             shape = RoundedCornerShape(
-              bottomStart = 10.dp,
-              bottomEnd = 10.dp
+              bottomStart = headerRadius,
+              bottomEnd = headerRadius
             )
           )
       ) { // header
@@ -88,28 +95,36 @@ fun App() {
             .height(60.dp)
           // .border(width = 2.dp, color = Color.Black)
         ) {
-          Text("Header")
+          Text(
+            text = "Feever",
+
+          )
         }
 
-        Row {
+        Row(
+          modifier = Modifier
+            .padding(start = headerRadius, end = headerRadius)
+            .fillMaxWidth()
+            .horizontalScroll(rememberScrollState()),
+          horizontalArrangement = Arrangement.Center
+        ) {
           val feedNames = remember {
             TestFeedConfigurator.getAllName()
           }
 
+          var count = 0
           feedNames.forEach { feedBName ->
-            Text(
-              text = feedBName,
-              modifier = Modifier
-                .drawBehind {
-                  drawLine(
-                    color = Color.Red,
-                    start = Offset(0f, size.height),
-                    end = Offset(size.width, size.height),
-                    strokeWidth = 2.dp.toPx()
-                  )
-                }
-                .padding(start = 0.dp)
-            )
+            val startPadding = 0.dp
+            var endPadding = 30.dp
+
+            if(count == feedNames.size - 1) {
+              endPadding = 0.dp
+            } else if(count == 0) {
+              AddFeedMenuItem("All", startPadding, endPadding, FontFamily(Font(manrope)))
+            }
+
+            AddFeedMenuItem(feedBName, startPadding, endPadding, FontFamily(Font(manrope)))
+            count++
           }
         }
       }
@@ -119,5 +134,37 @@ fun App() {
         Column { }
       }
     }
+  }
+}
+
+@Composable
+fun AddFeedMenuItem(text: String, startPadding: Dp, endPadding: Dp, fontFamily: FontFamily) {
+  Column(
+    modifier = Modifier
+      .padding(start = startPadding, end = endPadding),
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Image(
+      painterResource(Res.drawable.nullIcon),
+      modifier = Modifier
+        .size(80.dp)
+        .clip(CircleShape),
+      contentDescription = text
+    )
+    Text(
+      text = text,
+      modifier = Modifier
+        .drawBehind {
+          drawLine(
+            color = Color.Red,
+            start = Offset(0f, size.height),
+            end = Offset(size.width, size.height),
+            strokeWidth = 2.dp.toPx()
+          )
+        },
+      fontSize = 18.sp,
+      fontWeight = FontWeight(800),
+      fontFamily = fontFamily
+    )
   }
 }
--- composeApp/src/commonMain/kotlin/one/nfolio/feever/FeedConfigurator.kt
+++ composeApp/src/commonMain/kotlin/one/nfolio/feever/FeedConfigurator.kt
@@ -1,16 +1,21 @@
 package one.nfolio.feever
 
 interface FeedConfigurator {
-  fun registration(name: String, url: String)
+  fun registration(name: String, url: String, feedName: FeedTypes)
   fun getAllName() : List<String>
 }
 
 object TestFeedConfigurator : FeedConfigurator {
-  override fun registration(name: String, url: String) {
+  override fun registration(name: String, url: String, feedKind: FeedTypes) {
     TODO("Not yet implemented")
   }
 
   override fun getAllName(): List<String> {
-    return listOf("feed0", "feed1", "feed2", "feed3", "feed4")
+    return listOf("feed0", "feed1", "feed2", "feed3", "feed4", "feed5", "feed6")
   }
+}
+
+enum class FeedTypes {
+  RSS,
+  Atom
 }
